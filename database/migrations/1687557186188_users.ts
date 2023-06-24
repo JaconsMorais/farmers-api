cc40bb542091c7a1d0bfa9fefd7b3b7dbeb4e7b5
jaconsmorais <jaconssmorais@yahoo.com.br>
2023-06-24 18:38:49 -0400

    setup: Add remember_me_token in users table and setup uuid generation in db.

--- database/migrations/1687557186188_users.ts
+++ database/migrations/1687557186188_users.ts
@@ -4,9 +4,14 @@ export default class extends BaseSchema {
   protected tableName = 'users'
 
   public async up() {
+    await this.db.rawQuery('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+
     this.schema.createTable(this.tableName, (table) => {
+
       table.increments('id')
-      table.uuid('uuid').unique().notNullable().index()
+      table.uuid('uid').unique().notNullable().index().defaultTo(this.db.knexRawQuery('uuid_generate_v4()'))
+
+      table.string('remember_me_token').nullable()
 
       table.string('name', 100).notNullable()
       table.string('email', 50).notNullable().unique()
