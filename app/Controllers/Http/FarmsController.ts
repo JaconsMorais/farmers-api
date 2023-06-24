bdccf2096a89f37d519554a78c0820ad25d165a6
jaconsmorais <jaconssmorais@yahoo.com.br>
2023-06-24 18:35:23 -0400

    feat: Implement FarmsController.index method.

--- app/Controllers/Http/FarmsController.ts
+++ app/Controllers/Http/FarmsController.ts
@@ -1,17 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import Farm from 'App/Models/Farm'
 export default class FarmsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ }: HttpContextContract, farm: Farm) {
+    return { farm };
+  }
 
-  public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({ }: HttpContextContract) { }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ }: HttpContextContract, farm: Farm) {
+    return { farm };
+  }
 
-  public async edit({}: HttpContextContract) {}
 
-  public async update({}: HttpContextContract) {}
+  public async update({ }: HttpContextContract) { }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ }: HttpContextContract) { }
 }
