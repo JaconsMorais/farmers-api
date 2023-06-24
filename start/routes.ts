b5a833bd0c4cd503c2cbce653928b8aa2c873f69
jaconsmorais <jaconssmorais@yahoo.com.br>
2023-06-24 18:40:37 -0400

    feat: Add new route /users for UsersController

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,9 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.resource('users', 'UsersController').apiOnly().middleware({
+  'index': ['auth'],
+  'show': ['auth'],
+  'update': ['auth'],
+  'destroy': ['auth'],
 })
