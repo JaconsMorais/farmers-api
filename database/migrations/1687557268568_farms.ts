731a86cc4366be1553497bebb52039512f70fe86
jaconsmorais <jaconssmorais@yahoo.com.br>
2023-06-24 18:39:45 -0400

    setup: Add foreign keys for cultures and producers table in farms.

--- database/migrations/1687557268568_farms.ts
+++ database/migrations/1687557268568_farms.ts
@@ -6,12 +6,15 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.uuid('uuid').unique().notNullable().index()
+      table.uuid('uuid').unique().notNullable().index().defaultTo(this.db.knexRawQuery('uuid_generate_v4()'))
 
       table.string('name', 100).notNullable()
       table.decimal('area', 15, 2).notNullable()
       table.decimal('arableArea', 15, 2).notNullable()
       table.decimal('unusedArea', 15, 2).notNullable()
+
+      table.integer('cultureId').unsigned().references('id').inTable('cultures').onDelete('SET NULL')
+      table.integer('producerId').unsigned().references('id').inTable('producers').onDelete('CASCADE')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
