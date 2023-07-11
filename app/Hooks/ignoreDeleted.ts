b36719e1c3388231d2c8d5e8d0544c4409419af1
Jacons Morais <jaconssmorais@yahoo.com.br>
2023-07-11 12:45:04 -0400

    fix: Fix attribute name in ignoreDeleted hook.

--- app/Hooks/ignoreDeleted.ts
+++ app/Hooks/ignoreDeleted.ts
@@ -1,7 +1,7 @@
-import { ModelQueryBuilderContract } from "@ioc:Adonis/Lucid/Orm";
+import { ModelQueryBuilderContract } from '@ioc:Adonis/Lucid/Orm'
 
 export default (query: ModelQueryBuilderContract<any>) => {
-	if ('whereNull' in query) {
-		query.whereNull('deletedAt')
-	}
+  if ('whereNull' in query) {
+    query.whereNull('deleted_at')
+  }
 }
